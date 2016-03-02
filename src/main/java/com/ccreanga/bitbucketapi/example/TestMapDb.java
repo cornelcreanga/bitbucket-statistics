15bf0696f77189051fea51ba1e02c66be034fa9d
cornelcreanga@yahoo.com <tarnavenimures47>
2016-03-02 19:41:07 +0200

    add a small test for mapdb+kryo in order to replace ehcache (need persistent support between restarts)

---
+++ src/main/java/com/ccreanga/bitbucketapi/example/TestMapDb.java
@@ -0,0 +1,52 @@
+package com.ccreanga.bitbucketapi.example;
+
+import com.ccreanga.bitbucket.rest.client.model.User;
+import com.ccreanga.bitbucket.rest.client.model.UserType;
+import com.ccreanga.bitbucket.rest.client.model.pull.activity.PullRequestActivity;
+import com.ccreanga.bitbucket.rest.client.model.pull.activity.PullRequestMergeActivity;
+import com.ccreanga.bitbucket.rest.client.model.pull.activity.PullRequestOpenedActivity;
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Output;
+import org.mapdb.DB;
+import org.mapdb.DBMaker;
+import org.mapdb.HTreeMap;
+
+import java.io.File;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+public class TestMapDb {
+
+    public static void main(String[] args) {
+        DB db = DBMaker.newFileDB(new File("/tmp/testdb"))
+                .closeOnJvmShutdown()
+                .make();
+
+
+
+        DB.HTreeMapMaker mapMaker = db.createHashMap("bitBucket");
+        mapMaker.expireAfterWrite(2, TimeUnit.HOURS);
+        HTreeMap<String,byte[]> map = mapMaker.makeOrGet();
+        System.out.println(map.get("cucu").length);
+
+        Set<PullRequestActivity> activitySet = new HashSet<>();
+        activitySet.add(new PullRequestOpenedActivity(21L,new Date(),new User(1,"name1","email1","display1",true,"slug", UserType.NORMAL)));
+        activitySet.add(new PullRequestOpenedActivity(51L,new Date(),new User(1,"name2","email2","display2",true,"slug", UserType.NORMAL)));
+        activitySet.add(new PullRequestOpenedActivity(81L,new Date(),new User(1,"name3","email3","display3",true,"slug", UserType.NORMAL)));
+
+        Kryo kryo = new Kryo();
+        Output output = new Output(4096);
+        kryo.writeObject(output,activitySet);
+        output.close();
+        System.out.println(output.total());
+
+
+
+        map.put("cucu",output.toBytes());
+        db.commit();
+        db.close();
+    }
+
+}
